0bb8d4bdbf5c951b6d52c0dc90aa15a7f66c8ff3
slyvier <63706685+slyvier@users.noreply.github.com>
2020-04-19 11:53:14 +0700

    Update Russell-DS-PracticeCase2-Script.sql

--- Russell-DS-PracticeCase2-Script.sql
+++ Russell-DS-PracticeCase2-Script.sql
@@ -5,12 +5,12 @@ SELECT title FROM film WHERE  title LIKE '%astronaut%' OR title like '%space%';
 SELECT count(replacement_cost) as total FROM film WHERE rating = 'R' AND replacement_cost >=5 AND replacement_cost <=15;
 
 /*3*/
-select c.store_id,b.staff_id,count(a.amount) as payment_handled,sum(a.amount) as profit from payment a
+select c.store_id,b.staff_id,concat(b.first_name,' ',b.last_name) as nama_staff,count(a.amount) as payment_handled,sum(a.amount) as profit from payment a
 join
 staff b on a.staff_id =b.staff_id 
 join 
 store c on c.store_id =b.store_id 
-group by c.store_id,b.staff_id order by profit;
+group by c.store_id,b.staff_id,nama_staff order by profit;
 
 
 /*4*/
