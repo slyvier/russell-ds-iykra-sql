52773d2480c197e5ebc470486dea3ff9d64dc480
slyvier <63706685+slyvier@users.noreply.github.com>
2020-04-20 16:29:42 +0700

    Add files via upload
    Def to take input of name, address and date of birth and returning 1 single line of the value

---
+++ datadiri.py
@@ -0,0 +1,7 @@
+def datadiri():
+    name = input("Input Name : ")
+    address = input("Input Address : ")
+    birth = input("Input D.O.B : ")
+    print("My Name Is {}, I Live In {}, I Was Born On {}".format(name, address, birth))
+
+datadiri()
